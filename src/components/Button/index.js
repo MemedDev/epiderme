b67b51ea43b41d58231c8b1104d0e616570b5653
Carlos Silva <csilva2810@gmail.com>
2019-02-23 17:05:53 -0300

    feat(button): Add isSuccess prop to button component
    This modification is only for test semantic-release

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const Button = ({ children, isDanger }) => {
+const Button = ({ children, isDanger, isSuccess }) => {
   const buttonClass = classnames('button', {
     'button--is-danger': isDanger,
+    'button--is-success': isSuccess,
   });
 
   return (
@@ -14,11 +15,13 @@ const Button = ({ children, isDanger }) => {
 
 Button.defaultProps = {
   isDanger: false,
+  isSuccess: false,
 };
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   isDanger: PropTypes.bool,
+  isSuccess: PropTypes.bool,
 };
 
 export default Button;
